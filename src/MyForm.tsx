006f4c560a544824a35acd6a8ba5450333fcc0e2
marinadedev <marinadegames@gmail.com>
2022-06-25 16:09:52 +0300

    * validation message

--- src/MyForm.tsx
+++ src/MyForm.tsx
@@ -2,21 +2,16 @@ import {useState} from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
+const regexEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
 
 export const MyForm = () => {
-
-    const regexPassword = /[A-Za-z0-9]{8,}/
-    const regexEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
 
     const [name, setName] = useState<string>('')
     const [email, setEmail] = useState<string>('')
     const [tel, setTel] = useState<string>('')
     const [birthday, setBirthday] = useState(new Date());
-    const [message, setMessage] = useState<Date>()
+    const [message, setMessage] = useState<string>('')
 
-    const changeName = (e: string) => {
-        setName(e)
-    }
     const nameValidation = () => {
         const arr = name.replace(/\s+/g, ' ').trim().split(' ')
         if (arr.length === 2) {
@@ -32,7 +27,6 @@ export const MyForm = () => {
             console.error('Ошибка: Нужно ввести только имя и фамилию')
         }
     }
-
     const emailValidation = () => {
         if (regexEmail.test(email)) {
             console.log(email)
@@ -40,34 +34,40 @@ export const MyForm = () => {
             console.error('EMAIL WRONG!')
         }
     }
-
-
-
-
     const telValidate = () => {
         const regexpNumber = /^(\+7|7|8)?[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$/;
         if (regexpNumber.test(tel)) {
             console.log(tel)
-        }
-        else {
+        } else {
             console.error('ERROR: неверный формат номера!')
         }
     }
+    const messageValidation = () => {
+        if (message.length <= 300 && message.length > 9) {
+            console.log(message)
+        } else {
+            console.error('ERROR: текст сообщения должен быть мин 10 символов и макс 300 символов.')
+        }
+    }
 
+    const changeName = (e: string) => {
+        setName(e)
+    }
     const changeEmail = (e: string) => {
         setEmail(e)
     }
-
     const changeNumber = (e: string) => {
         setTel(e)
     }
-
     const changeBirthday = (e: any) => {
         setBirthday(e)
         console.log(e)
     }
+    const changeMessage = (e: string) => {
+        setMessage(e)
+    }
 
-    const sendMessage = () => {
+    const send = () => {
 
     }
 
@@ -77,16 +77,19 @@ export const MyForm = () => {
                 <div>
                     Имя и фамилия:
                     <input onBlur={nameValidation}
+                           value={name}
                            onChange={e => changeName(e.currentTarget.value)}/>
                 </div>
                 <div>
                     Email:
                     <input onBlur={emailValidation}
+                           value={email}
                            onChange={e => changeEmail(e.currentTarget.value)}/>
                 </div>
                 <div>
                     Телефон:
                     <input type="tel"
+                           value={tel}
                            name="tel"
                            onBlur={telValidate}
                            onChange={e => changeNumber(e.currentTarget.value)}/>
@@ -100,9 +103,13 @@ export const MyForm = () => {
                 </div>
                 <div>
                     Текст сообщения:
-                    <textarea/>
+                    <textarea value={message}
+                              onChange={e => changeMessage(e.currentTarget.value)}
+                              onBlur={messageValidation}
+                              maxLength={300}
+                              minLength={10}/>
                 </div>
-                <button onClick={sendMessage}>Send</button>
+                <button onClick={send}>Send</button>
             </form>
         </div>
     )
